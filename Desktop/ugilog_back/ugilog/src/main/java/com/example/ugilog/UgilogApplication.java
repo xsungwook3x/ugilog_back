4aa6912ef42e94416a33273133ceb228c2b41b07
Seonguk <xsungwook3x@naver.com>
2023-07-31 21:11:39 +0900

    start

---
+++ Desktop/ugilog_back/ugilog/src/main/java/com/example/ugilog/UgilogApplication.java
@@ -0,0 +1,13 @@
+package com.example.ugilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgilogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UgilogApplication.class, args);
+	}
+
+}
---
+++ Desktop/ugilog_back/ugilog/src/test/java/com/example/ugilog/UgilogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ugilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgilogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
